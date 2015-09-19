e431c3c12dfca3f073204f75bdd6b2a58e5efc60
Luke Iannini <lukexi@me.com>
2015-09-18 22:47:13 -0700

    Change the color of the cube that is reported by the raycast

--- test/Cubes.hs
+++ test/Cubes.hs
@@ -11,6 +11,8 @@ import Control.Monad.State
 import Control.Lens
 import Data.Maybe
 import Data.Map (Map)
+import qualified Data.Map as Map
+import System.Random
 
 import Types
 
@@ -18,12 +20,14 @@ import Physics.Bullet
 
 data World = World
   { _wldPlayer :: Pose
+  , _wldCubes  :: Map ObjectID Cube
   }
 makeLenses ''World
 
 newWorld :: World
 newWorld = World
     (Pose (V3 0 20 60) (axisAngle (V3 0 1 0) 0))
+    mempty
 
 main :: IO ()
 main = do
@@ -38,51 +42,65 @@ main = do
 
   dynamicsWorld  <- createDynamicsWorld mempty
   _              <- addGroundPlane dynamicsWorld (RigidBodyID 0) 0
-  cubeBodies     <- forM [1..1000] $ \i -> addCube dynamicsWorld (RigidBodyID i) mempty 
-    { pcPosition = V3 0 20 0
-    , pcRotation = Quaternion 0.5 (V3 0 1 1)
-    }
+  
 
   glEnable GL_DEPTH_TEST
 
   glClearColor 0 0 0.1 1
 
 
-  void . flip runStateT newWorld . whileWindow gpWindow $ do
-    processEvents gpEvents $ \e -> do
-      case e of
-        (MouseButton _ _ _) -> do mcubeId <- mapM getRigidBodyID =<< rayTestClosest dynamicsWorld =<< posToRay =<< getCursorPos gpWindow
-                                  case mcubeId of
-                                    (Just cubeId) -> liftIO $ putStrLn $ "Clicked Cube " ++ (show (unRigidBodyID cubeId))
-                                    otherwise -> return ()
-        otherwise           -> closeOnEscape gpWindow e
-        
-    applyMouseLook gpWindow wldPlayer
-    applyWASD gpWindow wldPlayer        
+  void . flip runStateT newWorld $ do 
+    forM_ [1..1000] $ \i -> do
+      rigidBody <- addCube dynamicsWorld (RigidBodyID i) mempty 
+        { pcPosition = V3 0 20 0
+        , pcRotation = Quaternion 0.5 (V3 0 1 1)
+        }
+      wldCubes . at (fromIntegral i) ?= Cube
+        { _cubBody = rigidBody
+        , _cubColor = V4 1 0 1 1
+        }
+    whileWindow gpWindow $ do
+      
+      processEvents gpEvents $ \e -> do
+        case e of
+          (MouseButton _ _ _) -> do mBodyID <- mapM getRigidBodyID =<< rayTestClosest dynamicsWorld =<< posToRay =<< getCursorPos gpWindow
+                                    case mBodyID of
+                                      Nothing -> return ()
+                                      Just bodyID -> do
+                                        liftIO $ putStrLn $ "Clicked Cube " ++ (show (unRigidBodyID bodyID))
 
-    stepSimulation dynamicsWorld
+                                        let cubeID = fromIntegral (unRigidBodyID bodyID)
+                                        [r,g,b] <- liftIO (replicateM 3 randomIO)
+                                        wldCubes . at cubeID . traverse . cubColor .= V4 r g b 1
+          otherwise           -> closeOnEscape gpWindow e
+          
+      applyMouseLook gpWindow wldPlayer
+      applyWASD gpWindow wldPlayer        
 
-    glClear (GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT)
+      stepSimulation dynamicsWorld
 
-    projMat <- makeProjection gpWindow
-    viewMat <- viewMatrixFromPose <$> use wldPlayer
+      glClear (GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT)
 
-    let viewProj = projMat !*! viewMat
+      projMat <- makeProjection gpWindow
+      viewMat <- viewMatrixFromPose <$> use wldPlayer
 
-    -- Begin cube batch
-    
-    withVAO (sVAO cubeShape) $ 
-      forM_ cubeBodies $ \rigidBody -> do
-        (position, orientation) <- getBodyState rigidBody
+      let viewProj = projMat !*! viewMat
 
-        let model = mkTransformation orientation position
-        uniformM44 uModelViewProjection (viewProj !*! model)
-        uniformM44 uInverseModel        (fromMaybe model (inv44 model))
-        uniformM44 uModel               model
-        uniformV4  uDiffuse             (V4 1 0 1 1)
-        glDrawElements GL_TRIANGLES (vertCount (sGeometry cubeShape)) GL_UNSIGNED_INT nullPtr
+      -- Begin cube batch
+      
+      withVAO (sVAO cubeShape) $ do
+        cubes <- Map.elems <$> use wldCubes
+        forM_ cubes $ \cube -> do
+          (position, orientation) <- getBodyState (cube ^. cubBody)
 
-    swapBuffers gpWindow
+          let model = mkTransformation orientation position
+          uniformM44 uModelViewProjection (viewProj !*! model)
+          uniformM44 uInverseModel        (fromMaybe model (inv44 model))
+          uniformM44 uModel               model
+          uniformV4  uDiffuse             (cube ^. cubColor)
+          glDrawElements GL_TRIANGLES (vertCount (sGeometry cubeShape)) GL_UNSIGNED_INT nullPtr
+
+      swapBuffers gpWindow
 
 
 posToRay (x, y) = do
