f84591a0d9d136aebb03b01b07179076c0abed56
Luke Iannini <lukexi@me.com>
2016-05-07 02:07:44 -0700

    Pass collision mask/group info to setRigidBodyShape

--- src/Physics/Bullet/RigidBody.hs
+++ src/Physics/Bullet/RigidBody.hs
@@ -41,52 +41,52 @@ addStaticRoom dynamicsWorld bodyID height = do
           , axisAngle (V3 1 0 0) (( pi)/2)
           , axisAngle (V3 0 1 0) ((-pi)/2)
           , axisAngle (V3 0 1 0) (( pi)/2)
-          , axisAngle (V3 0 1 0) (0) 
+          , axisAngle (V3 0 1 0) (0)
           , axisAngle (V3 0 1 0) (pi)
           ]
     forM_ rotations $ \rotation -> do
         collisionShape <- createStaticPlaneShape height
         addRigidBody dynamicsWorld bodyID collisionShape mempty { rbRotation = rotation, rbMass = 0 }
-        
+
     return ()
 
 addRigidBody :: (Functor m, MonadIO m) => DynamicsWorld -> CollisionObjectID -> CollisionShape -> RigidBodyConfig -> m RigidBody
-addRigidBody (DynamicsWorld dynamicsWorld) (fromIntegral -> rigidBodyID) (CollisionShape collisionShape) RigidBodyConfig{..} = liftIO $ 
+addRigidBody (DynamicsWorld dynamicsWorld) (fromIntegral -> rigidBodyID) (CollisionShape collisionShape) RigidBodyConfig{..} = liftIO $
     RigidBody . CollisionObject <$> [C.block| void * {
-      
+
         btDiscreteDynamicsWorld *dynamicsWorld = (btDiscreteDynamicsWorld *) $(void *dynamicsWorld);
         btCollisionShape        *shape         = (btCollisionShape *) $(void *collisionShape);
-      
-        // MotionStates are for communicating transforms between our engine and Bullet; 
+
+        // MotionStates are for communicating transforms between our engine and Bullet;
         // we're not using them
         // yet so we just use the btDefaultMotionState to set the initial object pose
         btQuaternion q = btQuaternion($(float qx), $(float qy), $(float qz), $(float qw));
         btVector3    p = btVector3($(float x), $(float y), $(float z));
-        
+
         btDefaultMotionState *motionState = new btDefaultMotionState(btTransform(q, p));
-      
+
         // Set the initial mass, inertia and restitiution
         btScalar mass     = $(float m);
         btVector3 inertia = btVector3($(float ix), $(float iy), $(float iz));
-      
+
         shape->calculateLocalInertia(mass, inertia);
-      
-        btRigidBody::btRigidBodyConstructionInfo rigidBodyCI( 
+
+        btRigidBody::btRigidBodyConstructionInfo rigidBodyCI(
             mass, motionState, shape, inertia);
         btRigidBody* rigidBody = new btRigidBody(rigidBodyCI);
-      
+
         rigidBody->setRestitution($(float r));
-      
+
         // Attach the given CollisionObjectID
         rigidBody->setUserIndex($(int rigidBodyID));
-      
-        dynamicsWorld->addRigidBody( 
-            rigidBody, 
-            $(short int rbCollisionGroup), 
+
+        dynamicsWorld->addRigidBody(
+            rigidBody,
+            $(short int rbCollisionGroup),
             $(short int rbCollisionMask) );
-        
+
         return rigidBody;
-      
+
         } |]
     where
       (V3 x y z)                    = realToFrac <$> rbPosition
@@ -94,7 +94,7 @@ addRigidBody (DynamicsWorld dynamicsWorld) (fromIntegral -> rigidBodyID) (Collis
       (Quaternion qw (V3 qx qy qz)) = realToFrac <$> rbRotation
       r                             = realToFrac     rbRestitution
       m                             = realToFrac     rbMass
-  
+
 removeRigidBody :: (Functor m, MonadIO m) => DynamicsWorld -> RigidBody -> m ()
 removeRigidBody (DynamicsWorld dynamicsWorld) (toCollisionObjectPointer -> rigidBody) = liftIO [C.block| void {
   btDiscreteDynamicsWorld* dynamicsWorld = (btDiscreteDynamicsWorld *) $(void *dynamicsWorld);
@@ -103,32 +103,34 @@ removeRigidBody (DynamicsWorld dynamicsWorld) (toCollisionObjectPointer -> rigid
   dynamicsWorld->removeRigidBody(rigidBody);
   delete rigidBody->getMotionState();
   delete rigidBody;
-  
+
   }|]
 
 -- | This is the only reliable way I've found so far to change an object's shape.
 -- see: http://bulletphysics.org/Bullet/phpBB3/viewtopic.php?t=5194
 -- If we try setLocalScaling again, see if the calculateLocalInertia/setMassProps helps anything, as I didn't try that.
-setRigidBodyShape :: (MonadIO m, Real a) => DynamicsWorld -> RigidBody -> CollisionShape -> a -> m ()
-setRigidBodyShape (DynamicsWorld dynamicsWorld) (toCollisionObjectPointer -> rigidBody) (CollisionShape collisionShape) mass = liftIO [C.block| void {
-  btDiscreteDynamicsWorld *dynamicsWorld = (btDiscreteDynamicsWorld *) $(void *dynamicsWorld);
-  btRigidBody             *rigidBody     = (btRigidBody *) $(void *rigidBody);
-  btCollisionShape        *shape         = (btCollisionShape *) $(void *collisionShape);
-  
-  dynamicsWorld->removeRigidBody(rigidBody);
-  
-  delete rigidBody->getCollisionShape();
-    
-  btScalar mass = $(float mass');
-  btVector3 inertia = btVector3(0,0,0);
-  shape->calculateLocalInertia(mass, inertia);
-  rigidBody->setCollisionShape(shape);
-  rigidBody->setMassProps(mass, inertia);
-  
-  dynamicsWorld->addRigidBody(rigidBody);
-  }|]
-  where
-    mass' = realToFrac mass
+setRigidBodyShape :: (MonadIO m, Real a) => DynamicsWorld -> RigidBody -> CollisionShape -> RigidBodyConfig -> m ()
+setRigidBodyShape (DynamicsWorld dynamicsWorld) (toCollisionObjectPointer -> rigidBody) (CollisionShape collisionShape) RigidBodyConfig{..} = liftIO [C.block| void {
+    btDiscreteDynamicsWorld *dynamicsWorld = (btDiscreteDynamicsWorld *) $(void *dynamicsWorld);
+    btRigidBody             *rigidBody     = (btRigidBody *) $(void *rigidBody);
+    btCollisionShape        *shape         = (btCollisionShape *) $(void *collisionShape);
+
+    dynamicsWorld->removeRigidBody(rigidBody);
+
+    delete rigidBody->getCollisionShape();
+
+    btScalar mass = $(float mass');
+    btVector3 inertia = btVector3(0,0,0);
+    shape->calculateLocalInertia(mass, inertia);
+    rigidBody->setCollisionShape(shape);
+    rigidBody->setMassProps(mass, inertia);
+
+    dynamicsWorld->addRigidBody(rigidBody,
+              $(short int rbCollisionGroup),
+              $(short int rbCollisionMask) );
+    }|]
+    where
+        mass' = realToFrac rbMass
 
 applyCentralImpulse :: (Functor m, MonadIO m, Real a) => RigidBody -> V3 a -> m ()
 applyCentralImpulse (toCollisionObjectPointer -> rigidBody) force = liftIO [C.block| void {
@@ -170,7 +172,7 @@ getBodyState (toCollisionObjectPointer -> rigidBody) = do
     // btTransform trans = rigidBody->getWorldTransform();
     btTransform trans;
     rigidBody->getMotionState()->getWorldTransform(trans);
-    
+
 
     btScalar *transformPtr = (btScalar *)malloc(sizeof(btScalar) * 7);
 
@@ -188,7 +190,7 @@ getBodyState (toCollisionObjectPointer -> rigidBody) = do
   } |]
 
   [x,y,z,qx,qy,qz,qw] <- liftIO $ withForeignPtr ptr (peekArray 7)
-  
+
   let position    = realToFrac <$> V3 x y z
       orientation = Quaternion (realToFrac qw) (V3 (realToFrac qx) (realToFrac qy) (realToFrac qz))
 
@@ -217,7 +219,7 @@ setRigidBodyKinematic (toCollisionObjectPointer -> rigidBody) (fromIntegral . fr
     // Restore the normal activation state (undoes DISABLE_DEACTIVATION)
     rigidBody->activate(true);
   }
-  
+
   }|]
 
 setRigidBodyNoContactResponse :: MonadIO m => RigidBody -> Bool -> m ()
@@ -232,7 +234,7 @@ setRigidBodyNoContactResponse (toCollisionObjectPointer -> rigidBody) (fromInteg
     rigidBody->setCollisionFlags(rigidBody->getCollisionFlags() &
                                  ~btCollisionObject::CF_NO_CONTACT_RESPONSE);
   }
-  
+
   }|]
 
 setRigidBodyDisableDeactivation :: MonadIO m => RigidBody -> Bool -> m ()
